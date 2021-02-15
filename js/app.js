33de8768820d53bdd8168f4147a645ad59b4f4b5
aloysious <alicia.todd.army@gmail.com>
2021-02-14 17:18:23 -0800

    event handling added images are not appearing still

--- js/app.js
+++ js/app.js
@@ -51,24 +51,30 @@ function getRandomIndex() {
   return Math.floor(Math.random() * allProducts.length);
 }
 
-function renderProduct(){
+function renderProduct() {
+  
   let productIndexArray = [];
+  
   while (productIndexArray.length < 3) {
+  
     let randomNumber = getRandomIndex();
-    while (!productIndexArray.includes(randomNumber)) {
+    
+  while (!productIndexArray.includes(randomNumber)) {
       productIndexArray.push(randomNumber);
     }
   }
-
+  
+  // [.includes](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes) *Developer.mozilla*
+  
   let firstProductIndex = productIndexArray.pop();
-  let secondProductIndex = productIndexArray.pop(lock-and-drop-it);                      
+  let secondProductIndex = productIndexArray.pop();
   let thirdProductIndex = productIndexArray.pop();
 
   // pop() is an inbuilt function in Python that removes and returns last value from the list or the given index value. Parameter : index (optional) - The value at index is popped out and removed. If the index is not given, then the last element is popped out and removed.
 
   // https://www.geeksforgeeks.org/python-list-pop/#:~:text=pop()%20is%20an%20inbuilt,or%20the%20given%20index%20value.&text=Parameter%20%3A,is%20popped%20out%20and%20removed.
 
-  imageOne.src = allProducts[firstProductIndex].src; 
+  imageOne.src = allProducts[firstProductIndex].src;
   imageOne.title = allProducts[firstProductIndex].name;
   allProducts[firstProductIndex].views++;
 
@@ -82,7 +88,9 @@ function renderProduct(){
 }
 
 function renderResults() {
+  
   let myList = document.querySelector('ul');
+  
   for (let i = 0; i < allProducts.length; i++) {
     let li = document.createElement('li');
     li.textContent = `${allProducts[i].name} had ${allProducts[i].votes} votes and was seen ${allProducts[i].views} times`;
@@ -90,77 +98,83 @@ function renderResults() {
   }
 }
 
-function handleClick(event){
+function handleClick(event) {
   totalClicks++;
+  if (event.target === myContainer) {
+  alert('Please click an image and FOLLOW INSTRUCTIONS');
+  
   let chosenProduct = event.target.title;
 
-  for (let i = 0; i < allProducts.length; i++){
-    if(chosenProduct === allProducts[i].name) {
+  for (let i = 0; i < allProducts.length; i++) {
+    if (chosenProduct === allProducts[i].name) {
       allProducts[i].votes++;
+    }
   }
-}
-console.log(sleeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeep!)
-// still need to handle the event stuff// dont forget button and rendering before assuming its broken >.< 
 
-// Add a button with the text View Results, which when clicked displays the list of all the products followed by the votes received, and number of times seen for each. Example: banana had 3 votes, and was seen 5 times.
+  function renderProduct() { 
+    if (totalClicks === clicksAllowed)
+      myContainer.removeEventListener('click', handleClick);
+      renderResults();
+    }
+  }
+  
+  function handleButtonClick(event) {   
+    if (totalClicks === clicksAllowed) {
+      renderResults();
+    }
+  }
+  renderProduct();
 
-// NOTE: Displayed product names should match the file name for the product. Example: the product represented with dog-duck.jpg should be displayed to the user as exactly “dog-duck” when the results are shown.
+  myContainer.addEventListener('click', handleClick);
+  myButton.addEventListener('click', handleButtonClick);
+  
+  
+  
+
+  
 
 
-// As a user, I would like to view a report of results after all rounds of voting have concluded so that I can evaluate which products were the most popular.
+  
 
-// - Create a property attached to the constructor function itself that keeps track of all the products that are currently being considered.
-// - After voting rounds have been completed, remove the event listeners on the product.
-// - Add a button with the text `View Results`, which when clicked displays the list of all the products followed by the votes received, and number of times seen for each. Example: `banana had 3 votes, and was seen 5 times.`
+
+
+  // const sentence = 'The quick brown fox jumps over the lazy dog.';
+// const word = 'fox';
+
+// console.log(`The word "${word}" ${sentence.includes(word) ? 'is' : 'is not'} in the sentence`);
+// expected output: "The word "fox" is in the sentence"
+
+
+
+
+
+
+  // still need to handle the event stuff// dont forget button and rendering before assuming its broken >.< 
+
+  // Add a button with the text View Results, which when clicked displays the list of all the products followed by the votes received, and number of times seen for each. Example: banana had 3 votes, and was seen 5 times.
+
+  // NOTE: Displayed product names should match the file name for the product. Example: the product represented with dog-duck.jpg should be displayed to the user as exactly “dog-duck” when the results are shown.
+
+
+  // As a user, I would like to view a report of results after all rounds of voting have concluded so that I can evaluate which products were the most popular.
+
+  // - Create a property attached to the constructor function itself that keeps track of all the products that are currently being considered.
+  // - After voting rounds have been completed, remove the event listeners on the product.
+  // - Add a button with the text `View Results`, which when clicked displays the list of all the products followed by the votes received, and number of times seen for each. Example: `banana had 3 votes, and was seen 5 times.`
 
   // - > NOTE: Displayed product names should match the file name for the product. Example: the product represented with `dog-duck.jpg` should be displayed to the user as exactly "dog-duck" when the results are shown.>
 
 
 
 
-renderProduct();
-// Attach an event listener to the section of the HTML page where the images are going to be displayed.
-myContainer.addEventListener('click', handleClick);
+  // Attach an event listener to the section of the HTML page where the images are going to be displayed. 
+  // make sure you make all the elements needed in HTML LOLZZZZZZZZZZZZZZZZ
 
 
 
 
 
-// -----------------CRUD BELOW IS DEMO CODE FROM GOATS---------------------------
-// SOME of it makes some sense some of it also makes none........................ ASK A CLASSMATE OR TA ABOUT LINE141
-
-// function handleClick(event){
-//   if (event.target === section){
-//     alert('Please click an image and FOLLOW INSTRUCTIONS');
-//     break;
-//   }
-//   totalClicks++;
-//   let goatClicked = event.target.title;
-
-//   for(let i = 0; i< allGoats.length; i++)
-//     if(goatClicked === allGoats[i].name) {
-//       allGoats[i].clicks++; 
-//     }
-//   }  
-
-//   renderGoats();
-//   if (totalClicks === clicksAllowed) {
-//     // REMOVE EVENT LISTENER
-//     myContainer.removeEventListener('click', handleClick);
-//     renderResults();
-//   }
-
-// }
-
-// function handleButtonClick(event){
-//   console.log('I was clicked');
-//   if(totalClicks === clicksAllowed){
-//     renderResults();
-//   }
-// }
-
-// renderGoats();
 
 
-// myContainer.addEventListener('click', handleClick);
-// myButton.addEventListener('click', handleButtonClick); 
+
+
